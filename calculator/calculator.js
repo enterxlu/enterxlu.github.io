11d5887530c045fa4d6a8e1b4eb430fc559b4eab
EnterX <36825968+enterxlu@users.noreply.github.com>
2020-10-26 10:42:33 -0400

    functional calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,77 @@
+let stored_num = 0;
+let current_num = 0;
+let current_operation = "";
+let display = document.getElementById("display");
+
+/*************************
+ * pressNum
+ * input: number clicked
+ * output: display number
+ *************************/
 function pressNum(digit) {
-    let display_text = document.getElementById("display").innerHTML;
-    if (display_text.length > 8) {
-        alert("Max Digits");
-        return;
-    }
-    let display_num = parseFloat(display_text);
-    display_num *= 10;
-    display_num += digit;
-    document.getElementById("display").innerHTML = display_num;
+    current_num *= 10;
+    current_num += digit;
+    display.innerHTML = current_num;
 }
+
+/*************************
+ * pressKeyClear
+ * output: reset to default
+ *************************/
+function pressKeyClear() {
+    stored_num = 0;
+    current_num = 0;
+    current_operation = "";
+    display.innerHTML = current_num;
+}
+
+/*************************
+ * pressKey
+ * input: operation clicked
+ * output: set current_operation as operation and press Equal
+ *************************/
+function pressKey(operation) {
+    pressKeyEqual();
+    current_operation = operation;
+    stored_num = current_num;
+    current_num = 0;
+}
+
+/*************************
+ * pressKeyEqual
+ * input: number clicked
+ * output: display number
+ *************************/
+function pressKeyEqual() {
+    function reset() {
+        stored_num = 0;
+        current_operation = "";
+    }
+    if (current_operation == "+") {
+        current_num += stored_num;
+        reset();
+    }
+    if (current_operation == "-") {
+        current_num = stored_num - current_num;
+        reset();
+    }
+    if (current_operation == "*") {
+        current_num *= stored_num;
+        reset();
+    }
+    if (current_operation == "/") {
+        current_num = stored_num / current_num;
+        reset();
+    }
+    display.innerHTML = current_num;
+}
+
+/**
+ * check if the digit is too much to be shown
+ */
+window.setInterval(function () {
+    if (display.innerHTML.length > 9) {
+        alert("Digit overflow");
+        pressKeyClear();
+    }
+}, 300);
