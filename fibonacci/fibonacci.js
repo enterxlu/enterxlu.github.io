b95107019e113dd05fbe9384c4d2281da9d925c8
EnterX <36825968+enterxlu@users.noreply.github.com>
2020-11-12 10:31:09 -0500

    golden ratio

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,9 +1,18 @@
-/*
-fib_sequence
-input: n is the number of the fibonacci numbers we will find
-output: list of the first n fibonacci numbers
-*/
 
+function updateWebpage() {
+    let n = document.getElementById("fib-length").value;
+    if (n > 1000) {
+        return document.getElementById("fib-length").value = "Too much!"
+    }
+    displayFibSequence(n, "sequence");
+    displayGoldenRatio(n, "golden-ratio");
+}
+
+/*
+* fib_sequence
+*input: n is the number of the fibonacci numbers we will find
+*output: list of the first n fibonacci numbers
+*/
 function getFibSequence(n) {
     let seq = [];
     let i;
@@ -21,11 +30,18 @@ function getFibSequence(n) {
     return seq;
 }
 
-console.log(getFibSequence(10));
-
-function displaySequence(n, div_id) {
+function displayFibSequence(n, div_id) {
     let fib = getFibSequence(n);
     document.getElementById(div_id).innerHTML = fib.join(", ");
 }
 
-displaySequence(42, "sequence");
+function getGoldenRatio(n) {
+    let f = getFibSequence(n);
+    if (n < 2) return 0;
+    return f[f.length - 1] / f[f.length - 2];
+}
+
+function displayGoldenRatio(n, div_id) {
+    let gr = getGoldenRatio(n);
+    document.getElementById(div_id).innerHTML = gr;
+}
