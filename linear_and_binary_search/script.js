af4283f9ab11a34a0da6ea875cc1fdb6792a7b81
EnterX <36825968+enterxlu@users.noreply.github.com>
2020-11-30 09:47:08 -0500

    1130

--- linear_and_binary_search/script.js
+++ linear_and_binary_search/script.js
@@ -1,4 +1,20 @@
 let list_of_numbers = [];
+let current_number_index = 10;
+let checked_index = [1, 2, 3, 4];
+let rejected_index = [10, 11, 12, 13, 14, 15];
+
+function buildList(list) {
+    let s = "";
+
+    for (let i = 0; i < list.length; i++) {
+        if (i == current_number_index) {
+            s = s + "<span class='current-index'>" + list[i] + " " + "</span>"
+        } else {
+            s = s + list[i] + " ";
+        }
+    }
+    return s;
+}
 
 function getListOfNumber() {
     let n = document.getElementById("list-length").value;
@@ -12,5 +28,7 @@ function getListOfNumber() {
         list_of_numbers.push(next_number);
     }
 
-    document.getElementById("number-list").innerHTML = list_of_numbers.join(" ")
+    current_number_index = Math.floor(n / 2);
+
+    document.getElementById("number-list").innerHTML = buildList(list_of_numbers);
 }
