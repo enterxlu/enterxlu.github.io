12a797c53e2eabf8c51d7872037ac654f87343c9
EnterX <36825968+enterxlu@users.noreply.github.com>
2020-12-01 09:19:49 -0500

    mark up and reset

--- linear_and_binary_search/script.js
+++ linear_and_binary_search/script.js
@@ -1,22 +1,66 @@
 let list_of_numbers = [];
 let current_number_index = 10;
-let checked_index = [1, 2, 3, 4];
-let rejected_index = [10, 11, 12, 13, 14, 15];
+let checked_index = [0, 1, 2, 3, 4];
+let rejected_index = [8, 9, 10];
 
+makeListOfNumbers();
+
+/**
+* checkInList(index, list)
+* input: index, single number
+* input: list, list of numbers
+* output: return true if the index exists in the list of numbers
+* otherwise return false
+*/
+function checkInList(index, list) {
+    for (let i = 0; i < list.length; i++) {
+        if (index == list[i]) {
+            return true;
+        }
+    }
+    return false;
+}
+
+/**
+ * buildList(list)
+ * input: list
+ * output: mark up current_number_index, checked_index and rejected_index 
+ */
 function buildList(list) {
     let s = "";
 
     for (let i = 0; i < list.length; i++) {
         if (i == current_number_index) {
             s = s + "<span class='current-index'>" + list[i] + " " + "</span>"
-        } else {
+        }
+        else if (checkInList(i, checked_index)) {
+            s = s + "<span class='checked-index'>" + list[i] + " " + "</span>"
+        }
+        else if (checkInList(i, rejected_index)) {
+            s = s + "<span class='rejected-index'>" + list[i] + " " + "</span>"
+        }
+        else {
             s = s + list[i] + " ";
         }
     }
     return s;
 }
 
-function getListOfNumber() {
+/**
+ * displayList(list)
+ * input: list
+ * display list on html
+ */
+function displayList(list) {
+    document.getElementById("number-list").innerHTML = buildList(list);
+}
+
+/**
+ * makeListOfNumbers()
+ * input: N/A
+ * output: call displayList to display a random list with the length number from input box
+ */
+function makeListOfNumbers() {
     let n = document.getElementById("list-length").value;
     let next_number = 0;
     let range = Math.floor(Math.random() * n + 10);
@@ -29,6 +73,17 @@ function getListOfNumber() {
     }
 
     current_number_index = Math.floor(n / 2);
+    displayList(list_of_numbers);
+}
 
-    document.getElementById("number-list").innerHTML = buildList(list_of_numbers);
+/**
+ * reset()
+ * input: N/A
+ * output: reset all variables
+ */
+function reset() {
+    current_number_index = -1;
+    checked_index = [];
+    rejected_index = [];
+    displayList(list_of_numbers);
 }
