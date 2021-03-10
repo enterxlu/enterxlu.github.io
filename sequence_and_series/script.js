abb1f90f44508c1edd3593569b08ebcf5af4e715
EnterX <36825968+enterxlu@users.noreply.github.com>
2021-03-10 12:22:38 -0500

    3/10

--- sequence_and_series/script.js
+++ sequence_and_series/script.js
@@ -1,81 +1,92 @@
 let sequence = [];
-let type;
 let length;
-let term1;
-let term2;
-let term3;
-let term4;
-let d1;
-let d2;
-let d3;
-let r1;
-let r2;
-let r3;
+let term = [];
+let d = [];
+let r = [];
 
-function runProgram() {
-    sequence = [];
-	document.getElementById("series").innerHTML = "";
-	document.getElementById("sequence").innerHTML = "";
+function main() {
     getTerms();
-    checkType();
-    createSequence();
-    sumSeries();
+	displayType();
+    displaySequence();
+	displaySeries();
+    displayFormula();
 }
 
 function getTerms() {
+	let term_id = ["term1","term2","term3","term4"];
     length = document.getElementById("length_of_sequence").value;
-    term1 = document.getElementById("term1").value;
-    term2 = document.getElementById("term2").value;
-    term3 = document.getElementById("term3").value;
-    term4 = document.getElementById("term4").value;
+	for (let i = 0; i<term_id.length;i++){
+		term[i] = document.getElementById(term_id[i]).value;
+	}
+}
+
+function displayType(){
+	if (checkType()=="Arithmetic"){
+	document.getElementById("type").innerHTML = checkType() + " with common difference of " + d[0];
+	} else if (checkType()=="Geometric"){
+	document.getElementById("type").innerHTML = checkType() + " with common ratio of " + r[0];
+	} else {
+		document.getElementById("type").innerHTML = checkType();
+	}
 }
 
 function checkType() {
-    d1 = term2 - term1;
-    d2 = term3 - term2;
-    d3 = term4 - term3;
-    r1 = term2 / term1;
-    r2 = term3 / term2;
-    r3 = term4 / term3;
-
-    if(d1 == d2 && d2 == d3) {
-        type = "A";
-        document.getElementById("type").innerHTML = "Arithmetic Sequence";
-    } else if(r1 == r2 && r2 == r3) {
-        type = "G";
-        document.getElementById("type").innerHTML = "Geometric Sequence";
-    } else {
-        type = "N";
-        document.getElementById("type").innerHTML = "Not Arithmetic or Geometric Sequence";
-    }
-}
-
-function createSequence() {
-    let term_now = parseFloat(term1);
-    if(type == "A") {
-        for(let i = 0; i < length; i++) {
-            sequence[i] = term_now;
-            term_now = term_now + d1;
-        }
-    } else if(type == "G") {
-        for(let i = 0; i < length; i++) {
-            sequence[i] = term_now;
-            term_now = term_now * r1;
-        }
-    }
-    document.getElementById("sequence").innerHTML = sequence.join(", ");
-}
-
-function sumSeries() {
-    let sum = 0;
-    for(let i = 0; i < sequence.length; i++) {
-        sum = sum + sequence[i];
-    }
-    if(type != "N") {
-        document.getElementById("series").innerHTML = sequence.join(" + ") + " = " + sum;
-    }
-}
-
-function getFormula(){
+    d[0]= term[1] - term[0];
+    d[1] = term[2] - term[1];
+    d[2] = term[3] - term[2];
+    r[0] = term[1] / term[0];
+    r[1] = term[2] / term[1];
+    r[2] = term[3] / term[2];
 	
+    if(d[0] == d[1] && d[1] == d[2]) {
+		return "Arithmetic";
+    } else if(r[0] == r[1] && r[1] == r[2]) {
+		return "Geometric";
+    } else {
+		return "None";
+    }
+}
+
+function displaySequence() {
+	document.getElementById("sequence").innerHTML = createSequence(term).join(", ");
+}
+
+function createSequence(term) {
+	sequence = [];
+    let term_now = parseInt(term[0]);
+    if(checkType() == "Arithmetic") {
+        for(let i = 0; i < length; i++) {
+            sequence[i] = term_now;
+            term_now = term_now + d[0];
+        }
+    } else if(checkType() == "Geometric") {
+        for(let i = 0; i < length; i++) {
+            sequence[i] = term_now;
+            term_now = term_now * r[0];
+        }
+    } 
+	return sequence;
+}
+
+function displaySeries() {
+	if(checkType() != "None") {
+        document.getElementById("series").innerHTML = sequence.join(" + ") + " = " + sumSeries(sequence);
+    }
+}
+
+function sumSeries(seq) {
+    let sum = 0;
+    for(let i = 0; i < seq.length; i++) {
+        sum = sum + seq[i];
+    }
+	return sum;
+}
+
+function displayFormula() {
+    let sup = "n-1";
+    if(checkType() == "Arithmetic") {
+        document.getElementById("formula").innerHTML = "[" + d[0] + " (n - 1) " + " + (" + sequence[1] + ")]";
+    } else if(checkType() == "Geometric") {
+        document.getElementById("formula").innerHTML = "[" + sequence[1] + " (" + r[0] + ")" + sup.sup() + "]";
+    }
 }
